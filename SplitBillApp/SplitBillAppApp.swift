9f17f4f5ab6a095842dc47e21ea8058219445f8d
adamrais <adamrais14@gmail.com>
2021-08-16 14:41:11 -0400

    Initial Commit

---
+++ SplitBillApp/SplitBillAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillAppApp.swift
+//  SplitBillApp
+//
+//  Created by Adam Rais on 2021-08-16.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
